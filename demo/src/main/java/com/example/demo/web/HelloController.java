44422a6ef5ad967e4f78b9e128970848bdcc69ff
Agustín Ranieri <agustinra@shifta.la>
2021-06-10 10:26:39 -0300

    005 - query params

--- demo/src/main/java/com/example/demo/web/HelloController.java
+++ demo/src/main/java/com/example/demo/web/HelloController.java
@@ -2,6 +2,7 @@ package com.example.demo.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,7 +21,7 @@ public class HelloController {
 
     @RequestMapping("/user_entry")
     public String userForm() {
-        return "<form action=\"/greeting/user_greeting\" method=\"POST\">\n" +
+        return "<form action=\"/greeting/user_greeting\" method=\"POST\">\n" + // method="GET" envía query params
                 "  <label for=\"fname\">First name:</label><br>\n" +
                 "  <input type=\"text\" id=\"fname\" name=\"fname\" value=\"John\"><br>\n" +
                 "  <label for=\"lname\">Last name:</label><br>\n" +
@@ -29,8 +30,9 @@ public class HelloController {
                 "</form>";
     }
 
+    // Si es GET, los parámetros se reciben via query
     @RequestMapping(value = "/user_greeting", method = RequestMethod.POST)
-    public String printUserGreeting() {
-        return "Form submitted!";
+    public String printUserGreeting(@RequestParam String fname, @RequestParam String lname) {
+        return "<h1>Hello there, " + fname + " " + lname + "</h1>";
     }
 }
