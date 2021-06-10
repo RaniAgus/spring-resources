d69d410471f39d91d397e9b00376a687b0bc088c
Agustín Ranieri <agustinra@shifta.la>
2021-06-10 10:20:35 -0300

    005 - post request

--- demo/src/main/java/com/example/demo/web/HelloController.java
+++ demo/src/main/java/com/example/demo/web/HelloController.java
@@ -8,8 +8,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/greeting")
 public class HelloController {
 
-    @RequestMapping(value = "", method = RequestMethod.GET)
+    @RequestMapping(value = "/basic", method = RequestMethod.GET)
     public String sayHello() {
         return "<h1>Hello</h1>";
+    }
+
+    @RequestMapping("/proper") // El method por default es GET
+    public String sayProperHello() {
+        return "<h1>Hello there, how are you?</h1>";
+    }
+
+    @RequestMapping("/user_entry")
+    public String userForm() {
+        return "<form action=\"/greeting/user_greeting\" method=\"POST\">\n" +
+                "  <label for=\"fname\">First name:</label><br>\n" +
+                "  <input type=\"text\" id=\"fname\" name=\"fname\" value=\"John\"><br>\n" +
+                "  <label for=\"lname\">Last name:</label><br>\n" +
+                "  <input type=\"text\" id=\"lname\" name=\"lname\" value=\"Doe\"><br><br>\n" +
+                "  <input type=\"submit\" value=\"Submit\">\n" +
+                "</form>";
+    }
+
+    @RequestMapping(value = "/user_greeting", method = RequestMethod.POST)
+    public String printUserGreeting() {
+        return "Form submitted!";
     }
 }
