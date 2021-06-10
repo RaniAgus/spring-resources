bd66103c761bb3ce8626048800d36d1dfd79feed
Agustín Ranieri <agustinra@shifta.la>
2021-06-10 13:05:12 -0300

    009 - uso de views html con thymeleaf

---
+++ demo/src/main/java/com/example/demo/web/HomeController.java
@@ -0,0 +1,25 @@
+package com.example.demo.web;
+
+import com.example.demo.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(Model model) {
+        // Model contiene los datos que se usan al interactuar con la vista mediante un map
+        model.addAttribute("formData", new User()); // key-value
+        return "index";
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public String processFormData(Model model, User user) { // Simplemente con mencionar el user ya lo tenemos
+        // En teoría no hace falta agregar el Model ni pasarle el User, pero creo que es mejor
+        model.addAttribute("user", user);
+        return "result";
+    }
+}
