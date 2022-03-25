68471fd733ee9dd657ac9801eefed8e8dbd1d951
Agustin Ranieri <aranieri@hexacta.com>
2022-03-25 10:48:10 -0300

    agrego HTTP 204 No Content response para DELETE

--- src/main/java/com/example/payroll/EmployeeController.java
+++ src/main/java/com/example/payroll/EmployeeController.java
@@ -89,7 +89,9 @@ class EmployeeController {
   }
 
   @DeleteMapping("/employees/{id}")
-  void deleteEmployee(@PathVariable Long id) {
+  ResponseEntity<?> deleteEmployee(@PathVariable Long id) {
     repository.deleteById(id);
+
+    return ResponseEntity.noContent().build();
   }
 }
